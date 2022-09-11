00020a6a8e8fd9db33341e5d028e3f1e734e7cbb
Iuliia Dolnikova <dolnikova80@gmail.com>
2022-09-11 19:51:47 +0300

    add hw 11(end)

--- src/js/fetchImages.js
+++ src/js/fetchImages.js
@@ -1,6 +1,6 @@
 export const fetchImages = async (inputValue, pageNr) => {
   return await fetch(
-    `https://pixabay.com/api/?key=29818369-26f2aeadd77818b7a67304d74=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
+    `https://pixabay.com/api/?key=29818369-26f2aeadd77818b7a67304d74&q=${inputValue}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${pageNr}`
   )
     .then(async response => {
       if (!response.ok) {
